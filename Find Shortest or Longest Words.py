ae22248a9a7375df4f72ef8c7f5989e31ee75aed
Sam <samanthahuang0823@gmail.com>
2021-10-09 21:50:31 -0400

    Function to find shortest or longest words

---
+++ Find Shortest or Longest Words.py
@@ -0,0 +1,24 @@
+# # creating an empty list
+new_list = []
+
+# a list of strings as input and split the strings
+words = [item for item in input("Enter the list items : ").split(',')]
+#print(words)
+def find_short_long_word(words_list):
+    shortest = None
+    longest = None
+    new_list = []
+    for i in words:
+        length = len(i)
+        #print(length)
+        if shortest is None:
+            shortest = i
+            new_list.append(i)
+        elif longest is None:
+            longest = i
+            new_list.append(i)
+        elif length > len(longest):
+            new_list[1] = i
+    return tuple(new_list)
+
+print(find_short_long_word(words)) #call the function

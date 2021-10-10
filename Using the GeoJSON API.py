39306b4601ab3ff5793c68caa921a2ed9012a7a2
samantha-huang <52383503+samantha-huang@users.noreply.github.com>
2021-10-10 16:25:35 -0400

    Delete Using the GeoJSON API.py

--- Using the GeoJSON API.py
+++
@@ -1,3 +0,0 @@
-#The program will prompt for a location, contact a web service and retrieve JSON for the web service
-# and parse that data, and retrieve the first place_id from the JSON. 
-# A place ID is a textual identifier that uniquely identifies a place as within Google Maps.
